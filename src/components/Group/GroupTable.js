e6ba94f65f9147714cfd25e3b7ff629f1395e823
taido028 <taibadboy028@gmail.com>
2023-07-03 19:00:13 +0200

    fix and jsdoc

--- src/components/Group/GroupTable.js
+++ src/components/Group/GroupTable.js
@@ -1,6 +1,14 @@
 import { AddingGroupButton } from "./AddingGroupButton";
 import { GroupTableRow } from "./GroupTableRow";
 import { GroupSearchBox } from "./GroupSearchBox";
+/**
+ * Component that displays a list of group members as a table.
+ * @component
+ * @param {Object} props - Component props
+ * @param {Object} props.page - The page object, which contains a list of groups
+ * @param {Object} props.actions - An object with actions that can be performed, passed to the GroupTableRow and AddingGroupButton components
+ * @returns {JSX.Element} A table with each row rendered by the GroupTableRow component and a button rendered by the AddingGroupButton component
+ */
 
 export const GroupTable = ({ page, actions }) => {
   return (
--- src/components/User/UserTable.js
+++ src/components/User/UserTable.js
@@ -1,6 +1,18 @@
 import { AddingUserButton } from "./AddingUserButton";
 import { UserTableRow } from "./UserTableRow";
 import { UserSearchBox } from "./UserSearchBox";
+/**
+ * Renders a table displaying a list of users. Each user has an id, name, surname,
+ * email, access level and associated actions. There's also an option to add a new user.
+ *
+ * @component
+ *
+ * @param {Object} props - The properties passed to this component.
+ * @param {Object} props.page - Page object which contains information about the users.
+ * @param {Object} props.actions - Actions which can be performed on a user.
+ *
+ * @returns {JSX.Element} JSX to create a table of users.
+ */
 
 export const UsersTable = ({ page, actions }) => {
   return (
